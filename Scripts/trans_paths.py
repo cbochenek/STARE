6be7a717fd29d611c1affaeb3993c2c1e0d5f34c
cbochenek <bochenekchristopher@gmail.com>
2018-09-28 14:40:14 -0700

    first commit

---
+++ Scripts/trans_paths.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+#
+# Heimdall python module for path inforamtion
+#
+
+def getControlDir():
+  return "/usr/local/control"
+
+def getConfigDir():
+  return "/usr/local/share"
+
+def getBinaryDir():
+  return "/usr/local/bin"
+
